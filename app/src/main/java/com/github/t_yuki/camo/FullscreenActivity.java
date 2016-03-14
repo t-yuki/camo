88195ee922e1a563dca842a89795cc99af420e6e
Yukinari Toyota <xxseyxx@gmail.com>
2016-03-14 20:41:23 +0900

    update

--- app/src/main/java/com/github/t_yuki/camo/FullscreenActivity.java
+++ app/src/main/java/com/github/t_yuki/camo/FullscreenActivity.java
@@ -183,7 +183,7 @@ public class FullscreenActivity extends Activity
             if (speed > 1.0f) {
                 speed = 1.0f;
             }
-            mRobot.drive(0f, speed); // TODO: which heading is front??
+//            mRobot.drive(0f, speed); // TODO: which heading is front??
             return in;
         }
         return in;
@@ -247,7 +247,11 @@ public class FullscreenActivity extends Activity
      * @param v The View that had been clicked
      */
     public void onControlClick(View v) {
-        // Find the heading, based on which button was clicked
+        if (mRobot == null) {
+            return
+        }
+
+            // Find the heading, based on which button was clicked
         final float heading;
         switch (v.getId()) {
 
@@ -277,7 +281,63 @@ public class FullscreenActivity extends Activity
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (mRobot == null) {
+            return;
+        }
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+onSensorChangedGyro(event);
+        }
+    }
 
+    // http://developer.android.com/intl/ja/guide/topics/sensors/sensors_motion.html#sensors-motion-accel
+    // Create a constant to convert nanoseconds to seconds.
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4]();
+    private float timestamp;
+
+    protected void onSensorChangedGyro(SensorEvent event){
+        // This timestep's delta rotation to be multiplied by the current rotation
+        // after computing it from the gyro sample data.
+        if (timestamp != 0) {
+            final float dT = (event.timestamp - timestamp) * NS2S;
+            // Axis of the rotation sample, not normalized yet.
+            float axisX = event.values[0];
+            float axisY = event.values[1];
+            float axisZ = event.values[2];
+
+            // Calculate the angular speed of the sample
+            float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+            // Normalize the rotation vector if it's big enough to get the axis
+            // (that is, EPSILON should represent your maximum allowable margin of error)
+            if (omegaMagnitude > EPSILON) {
+                axisX /= omegaMagnitude;
+                axisY /= omegaMagnitude;
+                axisZ /= omegaMagnitude;
+            }
+
+            // Integrate around this axis with the angular speed by the timestep
+            // in order to get a delta rotation from this sample over the timestep
+            // We will convert this axis-angle representation of the delta rotation
+            // into a quaternion before turning it into the rotation matrix.
+            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+            float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+            float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+            deltaRotationVector[0] = sinThetaOverTwo * axisX;
+            deltaRotationVector[1] = sinThetaOverTwo * axisY;
+            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+            deltaRotationVector[3] = cosThetaOverTwo;
+
+            float dy = deltaRotationVector[1];
+            // TODO: minimize dy
+        }
+        timestamp = event.timestamp;
+        float[] deltaRotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+        // User code should concatenate the delta rotation we computed with the current rotation
+        // in order to get the updated rotation.
+        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+    }
     }
 
     @Override
